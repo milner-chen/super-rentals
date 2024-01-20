fb58285ff5045139fe86c5ce44c2b75b08881955
Milner Chen <milnerchen0@gmail.com>
2024-01-19 19:24:55 -0500

    add tests to rental::image for size toggling

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'super-rentals/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function (hooks) {
@@ -19,6 +19,38 @@ module('Integration | Component | rental/image', function (hooks) {
       .exists()
       .hasAttribute('src', '/assets/images/teaching-tomster.png')
       .hasAttribute('alt', 'Teaching Tomster');
-  })
+  });
+
+  test('clicking component toggles its size', async function (assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    // check that there is a button with class image
+    assert.dom('button.image').exists();
+
+    // check that default state does not contain large
+    assert.dom('.image').doesNotHaveClass('large');
+    // small tag should contain this
+    assert.dom('.image small').hasText('View Larger');
+    
+    // click on the button -> maybe including button tag makes it more accessible?
+    await click('button.image');
+
+    // check that toggle succeeded -> check for large class
+    assert.dom('.image').hasClass('large');
+    // small tag check should have changed
+    assert.dom('.image small').hasText('View Smaller');
+
+    // toggle button again
+    await click('button.image');
+
+    // check it changed back to large
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+  });
   
 });
